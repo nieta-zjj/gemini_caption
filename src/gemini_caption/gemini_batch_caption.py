374d6381093ca72e61b1e1f081a5b607d47d2df4
shiertier <junjie.text@gmail.com>
2025-03-23 17:32:55 +0800

    update

--- src/gemini_caption/gemini_batch_caption.py
+++ src/gemini_caption/gemini_batch_caption.py
@@ -37,6 +37,11 @@ except ImportError:
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+file_handler = logging.FileHandler('gemini_caption.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
+
 caption_prompt_utils = CaptionPromptUtils()
 file_utils = FileUtils()
 
